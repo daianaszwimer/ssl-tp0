c40087f45aa5c766b5b23a3a38eb86dda94e0334
Nehuén <nehuen.esquivel@outlook.com>
2018-06-15 00:48:34 -0300

    Update scanner.c

--- scanner.c
+++ scanner.c
@@ -14,28 +14,24 @@ Espero que se entienda la idea, para mi que satisface la consigna*/
 char reconocer(){
 	char caracter;
 	char tipo = 'a';
-	do{ //defini un caracter con el que terminamos la cadena
+	char reconocer(){
+	char caracter;
+	char tipo = 'a';
+	do{
 		scanf("%c",&caracter);
-		if (tipo != 'e'){
-			if (caracter <= 57 && caracter >= 48){
-				if (tipo != 'i'){
-					tipo = 'c';
-				}
-			}else if ((caracter >= 65 && caracter <= 90) || (caracter <= 122 && caracter >= 97)){
-				tipo = 'i';
-			}else if (caracter == 32 || caracter == '\n'){//si hay espacio o es fin de entrada
-				//cuando hace este return, cuando vuelve a llamar
-				//por algun motivo no hace nada, entonces no hace el return f
-				//y se muere despues de esta linea cuando el caracter es el salto de linea
-				return tipo;
-			}else{
-				tipo = 'e';
-			}
-		}else if (caracter == 32 || caracter == '\n'){
-			tipo = 'a'; //hack para limpiar variable, buscar alternativa
-			return 'e';
-		}
-	}while(caracter != '\n');
-	//no llega aca arreglarlo
-	return 'f';		
-}
+	}while(caracter == 32)
+	if(caracter >= 48 && caracter <= 57){
+		tipo = 'c';
+	}else if((caracter >= 65 && caracter <= 90) || (caracter >= 97 && caracter <= 122)){
+		tipo = 'i';
+	}else{
+		tipo = 'e';
+	}	
+	do{
+		scanf("%c",&caracter);
+	}while(not (caracter == 32 || caracter == '\n'))
+	if(caracter == 32){
+		return tipo;
+	}else{
+		return 'f';		
+	}
