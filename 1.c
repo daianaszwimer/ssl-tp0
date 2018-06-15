ca43084cbe0ff3feed77c86687d83f1383b5adc2
Nehuén <nehuen.esquivel@outlook.com>
2018-06-15 00:42:52 -0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -2,7 +2,7 @@ char reconocer(){
 	char caracter;
 	char tipo = '';
 	
-	
+	scanf("%c",&caracter);
 	if(caracter >= 48 && caracter <= 57){
 		tipo = 'c';
 	}else if((caracter >= 65 && caracter <= 90) || (caracter >= 97 && caracter <= 122)){
@@ -13,50 +13,8 @@ char reconocer(){
 	do{
 		scanf("%c",&caracter);
 	}while(not (caracter == 32 || caracter == '\n'))
-	return tipo;
-		
-		
-		
-		
-		
-		
-		
-		
-			}else{
-				tipo = 'e';
-			}
-		
-		
-		
-		
-		
-	}while(caracter != '\n');	
-		
-	
-	do{	
-		
-		if (tipo != 'e'){
-			
-				if (tipo != 'i'){
-					
-					while 
-				}
-			
-			
-				tipo = 'i';
-			}else if (caracter == 32 || caracter == '\n'){//si hay espacio o es fin de entrada
-				//cuando hace este return, cuando vuelve a llamar
-				//por algun motivo no hace nada, entonces no hace el return f
-				//y se muere despues de esta linea cuando el caracter es el salto de linea
-				return tipo;
-			}else{
-				tipo = 'e';
-			}
-		}else if (caracter == 32 || caracter == '\n'){
-			tipo = 'a'; //hack para limpiar variable, buscar alternativa
-			return 'e';
-		}
-	
-	//no llega aca arreglarlo
-	return 'f';		
-}
+	if(caracter == 32){
+		return tipo;
+	}else{
+		return 'f';		
+	}
